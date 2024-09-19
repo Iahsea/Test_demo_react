3a5d78b879cdcb5d8d3fc0afee828663b56eddfe
Iahsea <ngyenchinhhai@gmail.com>
2024-09-19 10:41:28 +0700

    video 23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,17 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
   render() {
+    const myInnfor = ["ab", "abc", "a"];
     return (
       <div>
         <UserInfor />
+        <br />
+        <DisplayInfor name="Nguyen Chinh Hai" age="21" />
+        <hr />
+        <DisplayInfor name="Hai dep trai" age={20} myInnfor={myInnfor} />
       </div>
     );
   }
