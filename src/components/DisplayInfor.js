98625482af38e1610b13d644184c00d89fa403c3
Iahsea <ngyenchinhhai@gmail.com>
2024-09-20 10:28:49 +0700

    video 28

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./DisplayInfor.scss";
+import logo from "./../logo.svg";
 
 class DisplayInfor extends React.Component {
   state = {
@@ -16,6 +17,7 @@ class DisplayInfor extends React.Component {
     const { listUsers } = this.props;
     return (
       <div className="display-infor-container">
+        <img src={logo} />
         <div>
           <span
             onClick={() => {
@@ -35,9 +37,7 @@ class DisplayInfor extends React.Component {
                   key={user.id}
                   className={+user.age > 18 ? "green_color" : "red_color"}
                 >
-                  <div style={{ color: "yellow", paddingTop: "50px" }}>
-                    My name's: {user.name}
-                  </div>
+                  <div>My name's: {user.name}</div>
                   <div>My age's: {user.age}</div>
                   <hr />
                 </div>
