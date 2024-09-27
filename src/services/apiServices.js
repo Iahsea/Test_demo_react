e3ad75ba6a2ba93736eeff6bbb6186dd2b95dd45
Iahsea <ngyenchinhhai@gmail.com>
2024-09-28 00:23:15 +0700

    video 64

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -6,7 +6,7 @@ const postCreateNewUser = (email, password, username, role, image) => {
   data.append("password", password);
   data.append("username", username);
   data.append("role", role);
-  data.append("image", image);
+  data.append("userImage", image);
 
   return axios.post("api/v1/participant", data);
 };
@@ -15,4 +15,14 @@ const getAllUsers = () => {
   return axios.get("api/v1/participant/all");
 };
 
-export { postCreateNewUser, getAllUsers };
+const putUpdateUser = (id, username, role, image) => {
+  const data = new FormData();
+  data.append("id", id);
+  data.append("username", username);
+  data.append("role", role);
+  data.append("userImage", image);
+
+  return axios.put("api/v1/participant", data);
+};
+
+export { postCreateNewUser, getAllUsers, putUpdateUser };
