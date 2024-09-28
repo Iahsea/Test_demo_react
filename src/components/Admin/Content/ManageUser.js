e3205de96655e9d3959421c652a602c47e2a2930
Iahsea <ngyenchinhhai@gmail.com>
2024-09-28 12:24:14 +0700

    video 67

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -92,6 +92,7 @@ const ManageUser = (props) => {
           show={showModalDeleteUser}
           setShow={setShowModalDeleteUser}
           dataDelete={dataDelete}
+          fetchListUser={fetchListUser}
         />
       </div>
     </div>
